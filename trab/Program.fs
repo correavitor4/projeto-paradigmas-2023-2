3dd8bdfa79c4bae32b7f21a7e7fc74cec3d5fe87
CORREA DEEPIN OS <correavitor4@hotmail.com>
2023-09-28 15:44:46 -0300

    Primeira tentativa

--- trab/Program.fs
+++ trab/Program.fs
@@ -1,12 +1,34 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-let applyOperation opFunction x y = opFunction x y
+﻿open System
 
-let sum x y = x + y
-let multiply x y = x * y
+type BagItem = {
+    Value: double
+    Weight: float
+}
 
+let bagItems = [| { Value = 25.0; Weight = 1.5 };
+              { Value = 39.0; Weight = 2.0 };
+              { Value = 20.0; Weight = 1.2 };
+              { Value = 14.0; Weight = 0.9 };
+              { Value = 11.0; Weight = 1.45 }|]
 
-let sumResult = applyOperation sum 5 7
-let multiplyResult = applyOperation multiply 5 6
+//Ordena em ordem descrecente pela razão Valor/Peso
+let sortedItems =
+    Array.sortByDescending (fun item -> item.Value/item.Weight) bagItems
 
-printfn $"Resultado da soma: %d{sumResult}"
-printfn $"Resultado da multiplicação: %d{multiplyResult}"
+let maxBadWeight = 4
+
+let putItemsInBag (items: BagItem[]) (maxWeight: float) =
+    let mutable availableWeight = maxWeight
+    Array.fold (fun (acc: float) item ->
+            if item.Weight <= availableWeight then
+                availableWeight <- availableWeight - item.Weight
+                acc + item.Value
+            else
+                availableWeight <- 0.0
+                acc + item.Value * availableWeight
+        ) 0.0 items
+    
+let bagValue = putItemsInBag sortedItems maxBadWeight
+
+Console.WriteLine(bagValue)
+    
