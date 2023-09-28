52af1e395694866851b78a98204930788d6331ce
CORREA DEEPIN OS <correavitor4@hotmail.com>
2023-09-27 23:38:24 -0300

    First commit

---
+++ trab/Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+let applyOperation opFunction x y = opFunction x y
+
+let sum x y = x + y
+let multiply x y = x * y
+
+
+let sumResult = applyOperation sum 5 7
+let multiplyResult = applyOperation multiply 5 6
+
+printfn $"Resultado da soma: %d{sumResult}"
+printfn $"Resultado da multiplicação: %d{multiplyResult}"
